af37831b490b9477eb16026cfbdd684bcbced499
laila-chk <lyly.2356@gmail.com>
2022-06-21 18:08:40 +0100

    first cmt

---
+++ philo/main.c
@@ -0,0 +1,12 @@
+
+int main(int ac, char **av)
+{
+	if (ac > 4)
+	{
+
+	}
+	else
+		write(1, "Error!\nUsage:./philo number_of_philosophers time_to_dis
+					time_to_eat time_to_sleep
+					[number_of_times_each_philosopher_must_eat]");
+}
