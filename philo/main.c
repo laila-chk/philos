f9100cd298180cfd657551a45d2890ade7a52feb
Laila Chokri <lchokri@e1r11p8.1337.ma>
2022-08-22 23:05:44 +0100

    created threads

--- philo/main.c
+++ philo/main.c
@@ -6,13 +6,23 @@
 /*   By: lchokri <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/05 15:26:32 by lchokri           #+#    #+#             */
-/*   Updated: 2022/08/22 17:18:14 by lchokri          ###   ########.fr       */
+/*   Updated: 2022/08/22 23:05:39 by lchokri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philos.h"
 
-int		check_args(char **av, int count, int *philo)
+void	print_error(int i)
+{
+	if (i == 1)
+	{
+		write(2, "Error!\nUsage:./philo number_of_philosophers", 43);
+		write(2, "time_to_die time_to_eat time_to_sleep", 37);
+		write(2, "[number_of_times_each_philosopher_must_eat]\n", 44);
+	}
+}
+
+int	check_args(char **av, int count, int *philo)
 {
 	int	i;
 
@@ -31,16 +41,29 @@ int		check_args(char **av, int count, int *philo)
 	return (1);
 }
 
-void*	routine()
+void	*routine(void *arg)
 {
+	(void)arg;
 	printf("i will sleep now\n");
 	sleep(1);
 	printf("i will pretend to eat \n");
-	return ((void *)1);	
+	return ((void *)1);
+	//---------time to eat:
+	//mutex lock 
+	//usleep()
+	//mutex unlock
+	//usleep()
+	//~~~so we are basically locking a data (taking a fork) and using sleep to hold on there
+	//~then we unlock that data (let go of the fork) and go to sleep.
+	/**---- this one not gonna work----**/
+	/*
+	 *the idea is to use mutex_lock on mutexes of threads N+two so only those will access this
+	 *data and others wont
+	 *
+	 * */
 }
 
-// creating each thread:
-int		threads_creation(int num, pthread_t *th)
+int	threads_creation(int num, pthread_t *th)
 {
 	int		i;
 
@@ -49,12 +72,12 @@ int		threads_creation(int num, pthread_t *th)
 	{
 		if (pthread_create(&th[i], NULL, &routine, NULL))
 		{
-			printf("some problem had occured while creating the %dth thread\n", i + 1);
+			printf("problem occured while creating the %dth thread\n", i + 1);
 			return (0);
 		}
 		if (pthread_join(th[i], NULL))
 		{
-			printf("an Error occured while waiting for thread number %d to terminate\n", i + 1);
+			printf("Error while waiting for thread %d to terminate\n", i + 1);
 			return (0);
 		}
 		(num)--;
@@ -63,19 +86,21 @@ int		threads_creation(int num, pthread_t *th)
 	return (1);
 }
 
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
 	int			ph[5];
 	pthread_t	*th;
 
 	if (ac >= 5 && ac <= 6)
 	{
-		if(!check_args(av, ac, ph) || ph[0] == 0)
-			return (0);
+		if (!check_args(av, ac, ph) || ph[0] == 0)
+			return (1);
 		th = malloc(ph[0] * sizeof(pthread_t));
 		if (!threads_creation(ph[0], th))
-			return;
+			return (1);
+		//watcher thread here!
+		return (0);//kolchi smooth --> ret 1
 	}
-	else
-		write(2, "Error!\nUsage:./philo number_of_philosophers time_to_die time_to_eat time_to_sleep [number_of_times_each_philosopher_must_eat]", 126);
+	print_error(1);//khrejna mn if or madkholnalachii asln ==> kayn chi mochkil
+	return (1); 
 }
--- philo/philos.h
+++ philo/philos.h
@@ -1,17 +1,28 @@
-# ifndef PHILOS_H
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philos.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lchokri <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/22 17:53:32 by lchokri           #+#    #+#             */
+/*   Updated: 2022/08/22 17:54:58 by lchokri          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOS_H
 # define PHILOS_H
 
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
-#include <pthread.h>
+# include <pthread.h>
 
-int	positive_atoi(const char *str);
+int		positive_atoi(const char *str);
 size_t	ft_strlen(const char *s);
 
 typedef struct s_multi
 {
-	pthread_mutex_t mtx;
-//	void	forks;
-} t_multi;
+	pthread_mutex_t	mtx;
+}t_multi;
 #endif
