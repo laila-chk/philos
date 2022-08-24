76549c00a845d82487126e29dd977006f39a117b
Laila Chokri <lchokri@e1r5p9.1337.ma>
2022-08-24 06:06:24 +0100

    added a struct

--- philo/philos.h
+++ philo/philos.h
@@ -6,7 +6,7 @@
 /*   By: lchokri <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/22 17:53:32 by lchokri           #+#    #+#             */
-/*   Updated: 2022/08/22 17:54:58 by lchokri          ###   ########.fr       */
+/*   Updated: 2022/08/24 04:51:58 by lchokri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,24 @@
 int		positive_atoi(const char *str);
 size_t	ft_strlen(const char *s);
 
-typedef struct s_multi
+typedef	struct s_dt
 {
-	pthread_mutex_t	mtx;
-}t_multi;
+	//int		tot;
+	int		i;
+	int		t_die;
+	int		t_eat;
+	int		t_sleep;
+	int		meals;
+}t_dt;
+
+typedef struct s_gen
+{
+	int		tot;
+	int		vals[5];
+	pthread_mutex_t	*mtx;
+	pthread_t		*th;
+	t_dt			dt;
+
+}t_gen;
+
 #endif
