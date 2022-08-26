c1bf5a37c5d364ae754766b5e4fa5a987af469c4
Laila Chokri <lchokri@e1r11p10.1337.ma>
2022-08-26 13:27:07 +0100

    i'll change my struct

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,7 @@
 /*   By: lchokri <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/05 15:26:32 by lchokri           #+#    #+#             */
-/*   Updated: 2022/08/25 02:37:13 by lchokri          ###   ########.fr       */
+/*   Updated: 2022/08/26 13:15:56 by lchokri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,27 +43,65 @@ int	check_args(char **av, int count, t_gen *gen)
 
 //philos who will eat: N+2 then N+1
 //forks to lock:N+1
-void	eat(t_gen *gen)
+/*void	eat(t_gen *gen)
 {
 	int		i;
 
 	i = 0;
+//	while (i < gen->vals[0])
+//	{
+//		pthread_mutex_lock(&(gen->fork[i]));
+//		printf (".. taking fork number %d\n", i);
+//		i++;
+//	/
+	while (i < gen->vals[0] && gen->j % 2 == 0 && gen->vals[0] > 1)
+	{
+		pthread_mutex_lock(&(gen->fork[i]));
+		pthread_mutex_lock(&(gen->fork[i + 1]));
+		printf("%d < %d\n", gen->j, gen->vals[0]);
+		printf (".. taking fork number %d and %d\n", gen->j, gen->j+1);
+		printf("philosopher number %d is eating.......\n", gen->j);
+		usleep(gen->vals[2]);
+		gen->j+=2;
+	}
+	i = 0;
 	while (i < gen->vals[0])
 	{
-		pthread_mutex_lock(&(gen->mtx[i]));
-		printf (".. taking fork number %d\n", i);
-			i++;
+		pthread_mutex_unlock(&gen->fork[i++]);
+		printf("putting down fork number %d\n", i - 1);
 	}
 }
 
+void	eat(t_gen gen)
+{
+	pthread_mutex_lock(&(gen->fork[]));
+}
+*/
 void	*routine(void *arg)
 {
 	t_gen	*gen;
 
 	gen = ((t_gen*)arg);
-	printf("thread\n");
-	eat(gen);
+	printf("thread  number %d has entered\n", gen->j);
+//	while (1)
+//		eat(gen);
 	return ((void *)1);
+}
+
+void	struct_init(t_gen *gen)
+{
+	int	i;
+
+	i = 0;
+	gen->dt = malloc(gen->vals[0] * sizeof(gen->dt));
+	while (i < gen->vals[0])
+	{
+		gen->dt[i].i = i;// that's a fucking dumb ass idea
+		gen->dt[i].t_die = gen->vals[1];
+		gen->dt[i].t_sleep = gen->vals[3];
+		gen->dt[i].meals = gen->vals[4];
+		i++;
+	}
 }
 
 int	threads_creation(t_gen *gen)
@@ -71,24 +109,25 @@ int	threads_creation(t_gen *gen)
 	int		i;
 
 	i = 0;
-	while (gen->vals[0] > i)
+	pthread_mutex_init(gen->fork, NULL);
+	struct_init(gen);
+	while (gen->vals[0] > gen->j)
 	{
-		if (pthread_create(&(gen->th[i]), NULL, &routine, gen))
+		if (pthread_create(&(gen->th[(gen->j)]), NULL, &routine, gen))
 		{
-			printf("problem occured while creating the %dth thread\n", i + 1);
+			printf("problem occured while creating the %dth thread\n", gen->j);
 			return (0);
 		}
-		i++;
+	printf("___%d\n", gen->j);
+		(gen->j)++;
 	}
-	i = 0;
 	while (gen->vals[0] > i)
 	{
-		if (pthread_join((gen->th[i]), NULL))
+		if (pthread_join((gen->th[i++]), NULL))
 		{
-			printf("Error while waiting for thread %d to terminate\n", i + 1);
+			printf("Error while waiting for thread %d to terminate\n", i);
 			return (0);
 		}
-		i++;
 	}
 	return (1);
 }
@@ -97,14 +136,16 @@ int	main(int ac, char **av)
 {								//makefile wa9ila kay relinki!!
 	t_gen		gen;
 
+	gen.j = 0;
 	if (ac >= 5 && ac <= 6)
 	{
 		if (!check_args(av, ac, &gen) || gen.vals[0] == 0)
 			return (1);
 		gen.th = malloc(gen.vals[0] * sizeof(pthread_t));
-		gen.mtx= malloc(gen.vals[0] * sizeof(pthread_mutex_t));
+		gen.fork= malloc(gen.vals[0] * sizeof(pthread_mutex_t));
 		if (!threads_creation(&gen))
 			return (1);
+		pthread_mutex_destroy(gen.fork);
 		return (0);
 	}
 	print_error(1);
--- philo/philos.h
+++ philo/philos.h
@@ -6,7 +6,7 @@
 /*   By: lchokri <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/22 17:53:32 by lchokri           #+#    #+#             */
-/*   Updated: 2022/08/24 04:51:58 by lchokri          ###   ########.fr       */
+/*   Updated: 2022/08/26 13:16:16 by lchokri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,22 +23,51 @@ size_t	ft_strlen(const char *s);
 
 typedef	struct s_dt
 {
-	//int		tot;
 	int		i;
 	int		t_die;
-	int		t_eat;
 	int		t_sleep;
 	int		meals;
 }t_dt;
 
 typedef struct s_gen
 {
+	int		j;
 	int		tot;
 	int		vals[5];
-	pthread_mutex_t	*mtx;
+	pthread_mutex_t	*fork;
 	pthread_t		*th;
-	t_dt			dt;
+	t_dt			*dt;
 
 }t_gen;
+
+
+typedef struct s_shared
+{
+	int			i;
+	int			tot;
+	int			t_die;
+	int			t_eat;
+	int			t_sleep;
+	int			meals;
+	int			start;
+
+}	t_gen;
+
+typedef	struct	s_data;
+{
+	t_gen		gen;
+	pthread_t	*th;
+	int			n;
+}	t_dt;
+
+typedef	struct	*ph
+{
+	int				i;
+	int				start;
+	int				vals[5];
+	pthread_mutex_t	*fork;
+	pthread_t		*th;
+
+}	t_ph;
 
 #endif
