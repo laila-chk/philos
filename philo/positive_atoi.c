ec89fc9b1f277446473219870f1f97bd8eb5c251
Laila Chokri <lchokri@e1r10p6.1337.ma>
2022-09-13 03:07:38 +0100

    finished fr this time (i hope)

--- philo/positive_atoi.c
+++ philo/positive_atoi.c
@@ -18,23 +18,25 @@ int	positive_atoi(const char *str)
 	unsigned long	res;
 
 	i = 0;
-	res = 0;
+	res = -1;
 	while ((str[i] <= 13 && str[i] >= 9) || str[i] == 32)
 		i++;
 	if (str[i] == '-')
 	{
-		write(2, "Error! all values should be positive\n", 38);
+//		write(2, "Error! all values should be positive\n", 38);
 		return (-1);
 	}
 	if (str[i] == '+')
 		i++;
+	if (str[i] <= '9' && str[i] >= '0')
+		res = 0;
 	while (str[i] <= '9' && str[i] >= '0')
 		res = res * 10 + str[i++] - '0';
-	if (!(str[i] <= '9' && str[i] >= '0') && str[i])
+	if (str[i] && !(str[i] <= '9' && str[i] >= '0'))
 	{
-		write(2, "Error! ", 7);
-		write(2, str, ft_strlen(str));
-		write(2, " should be a positive number\n", 30);
+//		write(2, "Error! ", 7);
+//		write(2, str, ft_strlen(str));
+//		write(2, " should be a positive number\n", 30);
 		return (-1);
 	}
 	return (res);
@@ -47,11 +49,11 @@ int	check_args(char **av, int count, int *vals)
 	i = 0;
 	while (i <= count - 2)
 	{
-		if (av[i + 1][0] == '\0')
-		{
-			write(2, "Error: empty argument\n", 22);
-			return (0);
-		}
+		// if (av[i + 1][0] == '\0')
+		// {
+		// 	write(2, "Error: empty argument\n", 22)
+		// 	return (0);
+		// }
 		vals[i] = positive_atoi(av[i + 1]);
 		i++;
 	}
@@ -59,7 +61,10 @@ int	check_args(char **av, int count, int *vals)
 	while (i <= count - 2)
 	{
 		if (vals[i] == -1)
+		{
+			write(2, "Error: all values should be positive numbers\n", 45);
 			return (0);
+		}
 		i++;
 	}
 	if (count == 5)
